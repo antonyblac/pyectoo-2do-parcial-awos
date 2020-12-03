261812ff4533e1877b89808342dcbbe5cccf140f
rafael.ramirez <rafa.rami12.10@gmail.com>
2020-12-02 21:30:14 -0600

    Conexion lista a BD

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require('./config/config');
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
 
@@ -12,7 +13,7 @@ app.use(bodyParser.json());
 
 
 app.get('/', function(req, res) {
-    res.send('<h1>Bienvenido a mi servidor REST</h1>');
+    res.send('<h1>Bienvenido a mi servidor REST (localhost)</h1>');
 });
 app.get('/usuario', function(req, res) {
     res.jason({
@@ -64,6 +65,17 @@ app.delete('/Usuario/:id', function(req, res) {
         id: id
     });
 
+});
+
+mongoose.connect('mongodb://localhost:27017/cafeteria', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+
+}, (err, res) => {
+    if (err) throw err;
+    console.log('Base de datos ONLINE');
 });
 
 
