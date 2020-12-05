efd9abdb26dbb5ed8ff25c2cac466657954f3ded
rafael.ramirez <rafa.rami12.10@gmail.com>
2020-12-04 18:01:46 -0600

    Creada la cadena de conexion con mongo atlas en la nube

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(require('./routes/categoria'));
 app.use(require('./routes/login'));
 app.use(require('./routes/producto'));
 
-mongoose.connect('mongodb://localhost:27017/cafeteria', {
+mongoose.connect('mongodb+srv://admin:Cruzazul1@cluster0.ppjlk.mongodb.net/cafeteria', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
